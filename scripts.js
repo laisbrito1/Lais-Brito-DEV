8323b927457642bc236400ad40ad020d57eee7d0
Lais Brito <65972314+laisbrito1@users.noreply.github.com>
2023-08-02 11:24:12 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,67 @@
+
+//botão de copiar texto
+document.addEventListener('DOMContentLoaded', function() {
+    var emailElement = document.querySelector('.copy-email');
+    var copyButton = document.getElementById('copy-button');
+
+    emailElement.addEventListener('click', copyEmail);
+    copyButton.addEventListener('click', copyEmail);
+
+    function copyEmail() {
+        var emailAddress = 'lais_thauanne@outlook.com';
+
+        // Cria um elemento de input temporário
+        var tempInput = document.createElement('input');
+        tempInput.setAttribute('value', emailAddress);
+        document.body.appendChild(tempInput);
+
+        // Copia o valor do input para a área de transferência
+        tempInput.select();
+        document.execCommand('copy');
+
+        // Remove o elemento de input temporário
+        document.body.removeChild(tempInput);
+
+        // Altera o texto do botão após a cópia
+    var copyButton = document.getElementById('copy-button');
+    copyButton.innerHTML = 'Email copied!';}
+});
+
+//mudar cor do ccs
+
+
+const html = document.querySelector("#html");
+const checkbox= document.querySelector ("#switch");
+
+checkbox.addEventListener('change', function(){
+    html.classList.toggle("light-mode");
+})
+
+
+
+var originalText = ''; // Variable to store the original text
+var originalText2 = ''; // Variable to store the original text
+
+function mudarTexto() {
+    var elementoTexto = document.getElementById('texto');
+    var elementoTexto2 = document.getElementById('texto2');
+    var botao = document.getElementById('en');
+
+    if (botao.innerHTML === 'English') {
+        // Save the original text and change the button and element text to Portuguese
+        originalText = elementoTexto.innerHTML;
+        originalText2 = elementoTexto2.innerHTML;
+      
+        botao.innerHTML = 'Português';
+        elementoTexto.innerHTML = 'I am currently studying Computer Science at college and have been working as a freelancer for about 6 months. My specialties include HTML, CSS, JavaScript, SQL, Python, Windows, Figma, Canvas, Information Networks, and basic knowledge of JAVA.';
+        elementoTexto2.innerHTML = 'Illustrator | Designer';
+    } else {
+        // If the current text is 'Português', revert to English
+        botao.innerHTML = 'English';
+        elementoTexto.innerHTML = originalText;
+        elementoTexto2.innerHTML = originalText2;
+    }
+
+
+  
+}
